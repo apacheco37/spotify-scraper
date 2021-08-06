30bd905f0930ffe7aeead45e1381c27b98d34051
APacheco37 <apacheco73@hotmail.com>
2021-08-06 15:22:12 -0300

    Add site theme and colors

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import SearchBar from './components/SearchBar';
 import Results from './components/Results';
 import Header from './components/Header';
 import * as api from './api';
+import { ThemeProvider } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#1DB954', // https://usbrandcolors.com/spotify-colors/
+    },
+  },
+});
 
 class App extends React.Component {
   constructor(props) {
@@ -28,11 +43,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="site-padding">
-        <Header/>
-        <SearchBar onSearchTermSubmit={e => this.handleSearchTermSubmit(e)} />
-        <Results songs={this.state.songs} />
-      </div>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="site-padding">
+          <Header/>
+          <SearchBar onSearchTermSubmit={e => this.handleSearchTermSubmit(e)} />
+          <Results songs={this.state.songs} />
+        </div>
+      </ThemeProvider>
     );
   }
 }
